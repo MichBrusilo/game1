c9d0b93b79989afa4f7cd2d760b20b93d5626137
Michał Brusiło <mibru@interia.pl>
2020-02-19 11:10:45 +0100

    finish first part of Draw.js

--- Draw.js
+++ Draw.js
@@ -1,5 +1,18 @@
 class Draw {
     constructor() {
-        
+        this.options = ['green', 'red', 'blue'];
+        let _result = this.drawResult;
+        this.getDrawResult = () => _result;
+    }
+    drawResult() {
+        let colors = [];
+        for(i = 0; i < this.options.length; i++) {
+            const index = Math.floor(Math.random() * this.options.length);
+            const color = this.options[index];
+            colors.push(color);
+        }
+        return colors
     }
 }
+
+// const draw = new Draw();
