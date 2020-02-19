22f30f8fc679a8fac8f19f08ff96aa28fcaa6430
Michał Brusiło <mibru@interia.pl>
2020-02-19 10:46:33 +0100

    work with statistics.js

--- Statistics.js
+++ Statistics.js
@@ -1,3 +1,21 @@
 class Statistics {
-    
+    constructor() {
+        this.gameResults = [];
+    }
+
+    addGameToStatistic(win, bid) {
+        let gameResult = {
+            win: win,
+            bid: bid
+        }
+        this.gameResults.push(gameResult)
+    }
+
+    showGameStatistics() {
+        let games = this.gameResults.length;
+        //try to change below in the future
+        let wins = this.gameResults.filter(result => result.win).length;
+        let losses = this.gameResults.filter(result => !result.win).length;
+        return [games, wins, losses];
+    }
 }
