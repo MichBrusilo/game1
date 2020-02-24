4e2d167ba68c7bcb52443eca2b3d13aa38e7c368
Michał Brusiło <mibru@interia.pl>
2020-02-24 09:45:48 +0100

    build startgame()

--- game.js
+++ game.js
@@ -5,7 +5,7 @@ class Game {
         this.stats = new Statistics();
         this.wallet = new Wallet(start);
 
-        document.getElementById('start').addEventListener('click', this.startGame);
+        document.getElementById('start').addEventListener('click', this.startGame.bind(this));
         this.spanWallet = document.querySelector('.panel span.wallet');
         this.boards = [...document.querySelectorAll('div.color')];
         this.inputBid = document.getElementById('bid');
@@ -17,13 +17,18 @@ class Game {
         this.render()
     }
 
-    render(colors = ['gray', 'gray', 'gray'], money = this.wallet.getWalletValue(), result = "", stats = [0, 0, 0]) {
+    render(colors = ['gray', 'gray', 'gray'], money = this.wallet.getWalletValue(), result = "", stats = [0, 0, 0], wonMoney = 0, bid = 0) {
 
         this.boards.forEach((board, i) => {
             board.style.backgroundColor = colors[i]
         });
 
         this.spanWallet.textContent = money;
+        if(result) {
+            result = `Wygrałeś ${wonMoney}`;
+        } else if (!result && result != "") {
+            result = `Przegrałeś ${bid}`;
+        }
         this.spanResult.textContent = result;
         this.spanGames.textContent = stats[0];
         this.spanWins.textContent = stats[0];
@@ -32,7 +37,12 @@ class Game {
     }
 
     startGame() {
+        if(this.inputBid.value < 1) return alert('Kwota za mała!')
+        const bid = Math.floor(this.inputBid.value);
 
+        if(!this.wallet.checkCanPlay(bid)) {
+            return alert('Za mało środków lub zła wartość!')
+        }
     }
 }
 
